35259a8d833a712ed468431d20999a47d02ab16f
unknown <1905185677@qq.com>
2025-01-04 13:07:20 +0800

    modify

--- WB.v
+++ WB.v
@@ -2,12 +2,13 @@
 module WB(
     input wire clk,
     input wire rst,
-    // input wire flush,
+    input wire flush,
     input wire [`StallBus-1:0] stall,
 
     input wire [`MEM_TO_WB_WD-1:0] mem_to_wb_bus,
 
     output wire [`WB_TO_RF_WD-1:0] wb_to_rf_bus,
+    output wire [65:0] hilo_bus,
 
     output wire [31:0] debug_wb_pc,
     output wire [3:0] debug_wb_rf_wen,
@@ -21,9 +22,9 @@ module WB(
         if (rst) begin
             mem_to_wb_bus_r <= `MEM_TO_WB_WD'b0;
         end
-        // else if (flush) begin
-        //     mem_to_wb_bus_r <= `MEM_TO_WB_WD'b0;
-        // end
+        else if (flush) begin
+            mem_to_wb_bus_r <= `MEM_TO_WB_WD'b0;
+        end
         else if (stall[4]==`Stop && stall[5]==`NoStop) begin
             mem_to_wb_bus_r <= `MEM_TO_WB_WD'b0;
         end
@@ -31,23 +32,25 @@ module WB(
             mem_to_wb_bus_r <= mem_to_wb_bus;
         end
     end
-
+    
     wire [31:0] wb_pc;
     wire rf_we;
     wire [4:0] rf_waddr;
     wire [31:0] rf_wdata;
 
     assign {
-        wb_pc,
-        rf_we,
-        rf_waddr,
-        rf_wdata
+        hilo_bus,        // 135:70
+        wb_pc,           // 69:38
+        rf_we,           // 37
+        rf_waddr,        // 36:32
+        rf_wdata         // 31:0
     } = mem_to_wb_bus_r;
 
+    // assign wb_to_rf_bus = mem_to_wb_bus_r[`WB_TO_RF_WD-1:0];
     assign wb_to_rf_bus = {
-        rf_we,
-        rf_waddr,
-        rf_wdata
+        rf_we,           // 37
+        rf_waddr,        // 36:32
+        rf_wdata         // 31:0
     };
 
     assign debug_wb_pc = wb_pc;
